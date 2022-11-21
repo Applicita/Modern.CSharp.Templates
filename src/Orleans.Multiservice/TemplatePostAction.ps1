e489779db22fed255622d2965f0a5c0c0442a696
VincentH.NET <dev@vincenth.net>
2022-11-21 10:39:02 +0100

    include parameter in usage description for AddLogicalService.ps1 (Orleans.Multiservice template)

--- src/Orleans.Multiservice/TemplatePostAction.ps1
+++ src/Orleans.Multiservice/TemplatePostAction.ps1
@@ -1,5 +1,5 @@
 Start-Sleep -seconds 1 # Workaround to prevent exception from lock on dotnet.exe log file when nesting dotnet.exe invocations (log filename includes current time down to seconds)
 dotnet add Apis/Apis.csproj reference LogicalserviceNameService/LogicalserviceNameService.csproj
 
-Write-Host "Use AddLogicalService.ps1 to add more logical services" -ForegroundColor White -BackgroundColor DarkBlue
+Write-Host "Use AddLogicalService.ps1 <name> to add more logical services" -ForegroundColor White -BackgroundColor DarkBlue
 Remove-Item $PSCommandPath -Force
