e3d2feeb75a2ef55804ed9711f2ea3dd36572dbb
VincentH.NET <dev@vincenth.net>
2023-02-16 15:07:33 +0100

    oops

--- src/Uno/UnoWinUIQuickStart/Views/MainPage.cs
+++ src/Uno/UnoWinUIQuickStart/Views/MainPage.cs
@@ -10,7 +10,6 @@ partial class MainPage
     public void BuildUI() => this.Content(
         Grid(
             TextBlock("Hello, world from C# Markup!") .Margin (20) .FontSize (30)
-            new UI.
         )
     )  .Background (Black);
 }
