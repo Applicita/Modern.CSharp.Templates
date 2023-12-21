cdea3f3882ce3d2735425d8a1f13973991e94157
VincentH.NET <dev@vincenth.net>
2023-12-21 16:13:37 +0100

    uno view template fixes

--- src/Uno-View/Mvvm/ViewNameViewModel.cs
+++ src/Uno-View/Mvvm/ViewNameViewModel.cs
@@ -6,5 +6,8 @@ public partial class ViewNameViewModel
     public string Message { get; set; } = "Hello from C# Markup 2 - the concise, next-gen C# Markup for Uno!";
 }
 #else
-public partial class ViewNameViewModel(string Message = "NJoy concise C# Markup 2 for Uno Platform");
+public partial class ViewNameViewModel(string message = "NJoy concise C# Markup 2 for Uno Platform")
+{
+    public string Message => message;
+}
 #endif
